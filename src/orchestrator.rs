051e871091fd816f684e31ab193d988b199ec90c
Dan Miller <dan@dmiller.dev>
2020-09-16 13:43:58 -0400

    this compiles

--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -1,16 +1,12 @@
 // NOTE(dmiller): I'm confused why I have to do this
 #[path = "runner.rs"] mod runner;
 
-pub trait Orchestrator: Sized {
-    fn handle_event(&self, event: notify::Event);
+pub struct Orchestrator<'a> {
+    build: &'a dyn runner::Runner,
 }
 
-pub struct TestOrchestrator {
-    build: runner::Runner,
-}
-
-impl Orchestrator for TestOrchestrator {
-    fn handle_event(&self, event: notify::Event) {
+impl Orchestrator<'_> {
+    pub fn handle_event(&self, event: notify::Event) {
         println!("{:?}", event);
     }
 }
