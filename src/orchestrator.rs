9af7abb71d872497cebcc1300c7d69d892143fb6
Dan Miller <dan@dmiller.dev>
2020-09-23 12:55:44 -0400

    omg the tests pass

--- src/orchestrator.rs
+++ src/orchestrator.rs
@@ -13,15 +13,19 @@ pub struct Orchestrator<'a> {
     revert: &'a dyn Runner,
 }
 
-
-fn cmd_failed(output: std::result::Result<std::process::Output, std::io::Error>) -> Option<std::io::Error> {
+fn cmd_failed(
+    output: std::result::Result<std::process::Output, std::io::Error>,
+) -> Option<std::io::Error> {
     match output {
         Ok(res) => {
             if !res.status.success() {
-                return Some(Error::new(ErrorKind::Other, "cmd returned non-zero exit code"));
+                return Some(Error::new(
+                    ErrorKind::Other,
+                    "cmd returned non-zero exit code",
+                ));
             }
             return None;
-        },
+        }
         Err(e) => {
             return Some(e);
         }
@@ -32,15 +36,21 @@ impl Orchestrator<'_> {
     // TODO(dmiller): in the future this should take a notify event, or a list of changed paths or something
     pub fn handle_event(&self) -> std::result::Result<(), std::io::Error> {
         let build = self.build.run();
-        if cmd_failed(build).is_some() {
-            self.run_revert();
+        match cmd_failed(build) {
+            Some(err) => {
+                self.run_revert();
+                return Ok(());
+            }
+            None => {}
         }
-
         let test = self.test.run();
-        if cmd_failed(test).is_some() {
-            self.run_revert();
+        match cmd_failed(test) {
+            Some(err) => {
+                self.run_revert();
+                return Ok(());
+            }
+            None => {}
         }
-
 
         let commit = self.commit.run();
         return Ok(());
@@ -62,24 +72,24 @@ impl Orchestrator<'_> {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn it_works() {
-    //     assert_eq!(2 + 2, 4);
-    // }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 
-    // #[test]
-    // fn test_mock_runner() {
-    //     let mut mock = MockRunner::default();
-    //     mock.expect_run()
-    //         .returning(|| std::process::Command::new("true").output());
-    //     mock.run().expect("not to fail");
-    // }
+    #[test]
+    fn test_mock_runner() {
+        let mut mock = MockRunner::default();
+        mock.expect_run()
+            .returning(|| std::process::Command::new("true").output());
+        mock.run().expect("not to fail");
+    }
 
     fn fail() -> MockRunner {
         let mut fail = MockRunner::default();
         fail.expect_run()
             .times(1)
-            .returning(|| std::process::Command::new("exit 1").output());
+            .returning(|| std::process::Command::new("false").output());
 
         return fail;
     }
@@ -123,21 +133,21 @@ mod tests {
         orc.handle_event().expect("This shouldn't error");
     }
 
-    //#[test]
-    // fn test_orchestrator_build_succeeds_test_fails() {
-    //     let build = succeed();
-    //     let test = fail();
+    #[test]
+    fn test_orchestrator_build_succeeds_test_fails() {
+        let build = succeed();
+        let test = fail();
 
-    //     let commit = not_called();
-    //     let revert = called();
+        let commit = not_called();
+        let revert = called();
 
-    //     let orc = Orchestrator {
-    //         build: &build,
-    //         test: &test,
-    //         commit: &commit,
-    //         revert: &revert,
-    //     };
+        let orc = Orchestrator {
+            build: &build,
+            test: &test,
+            commit: &commit,
+            revert: &revert,
+        };
 
-    //     orc.handle_event().expect("This shouldn't error");
-    // }
+        orc.handle_event().expect("This shouldn't error");
+    }
 }
